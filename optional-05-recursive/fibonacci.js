09481312d8bf416a5b9aa39999e2eab391f01492
Muhammad Nouval Ar-Rizqy <bagobago955@gmail.com>
2026-03-18 17:04:50 +0700

    Update fibonacci.js

--- optional-05-recursive/fibonacci.js
+++ optional-05-recursive/fibonacci.js
@@ -1,5 +1,19 @@
 function fibonacci(n) {
-
+  // Kondisi berhenti untuk elemen 0 dan 1
+  if (n === 0) {
+    return [0];
+  }
+  if (n === 1) {
+    return [0, 1];
+  }
+  
+  // Rekursif: mengambil deret sebelumnya
+  const prevFib = fibonacci(n - 1);
+  
+  // Menambahkan angka baru di akhir deret
+  prevFib.push(prevFib[prevFib.length - 1] + prevFib[prevFib.length - 2]);
+  
+  return prevFib;
 }
 
 // Jangan hapus kode di bawah ini!
