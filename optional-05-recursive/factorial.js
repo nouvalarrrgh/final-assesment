4f105e965c010947dab915efb83b4d8ae2bb9eef
Muhammad Nouval Ar-Rizqy <bagobago955@gmail.com>
2026-03-18 17:04:35 +0700

    Update factorial.js

--- optional-05-recursive/factorial.js
+++ optional-05-recursive/factorial.js
@@ -1,5 +1,10 @@
 function factorial(n) {
-
+  // Kondisi berhenti
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  // Rekursif
+  return n * factorial(n - 1);
 }
 
 // Jangan hapus kode di bawah ini!
